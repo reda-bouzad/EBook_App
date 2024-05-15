54ded82c5e19dd40869bb8ef0c9a83454fb34b13
reda-bouzad <redabouzadfst@gmail.com>
2024-05-15 22:32:54 +0100

    added Popular Books scrollable

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'app_colors.dart' as AppColors;
 
@@ -9,17 +11,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyHomePage> {
+  List? popularBooks = [];
+  readData() async {
+    await DefaultAssetBundle.of(context)
+        .loadString("json/popularBooks.json")
+        .then((s) => setState(() {
+              popularBooks = json.decode(s);
+            }));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: const Color.fromARGB(255, 16, 16, 92),
-      child: const SafeArea(
+      child: SafeArea(
         child: Scaffold(
           body: Column(
             children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10, left: 20, right: 20),
+                child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.dashboard),
+                      Row(
+                        children: [
+                          Icon(Icons.search),
+                          SizedBox(width: 10),
+                          Icon(Icons.notifications)
+                        ],
+                      ),
+                    ]),
+              ),
+              const SizedBox(height: 20),
               Row(
-                children: [Icon(Icons.search), Icon(Icons.notifications)],
-              )
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child: const Text(
+                      "Popular Books",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 180,
+                child: PageView.builder(
+                    controller: PageController(viewportFraction: 0.7),
+                    itemCount: popularBooks?.length ?? 0,
+                    itemBuilder: (_, i) {
+                      return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 180,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                  image: AssetImage(popularBooks?[i]["img"])),
+                            ),
+                          ));
+                    }),
+              ),
             ],
           ),
         ),
